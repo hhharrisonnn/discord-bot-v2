4f0422a3fb50c9eb50a17d5f966d7bf9bcca5f98
hhharrisonnn <harrison.ho19@gmail.com>
2021-08-21 02:53:26 +0100

    Added bot's activity

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,3 +1,4 @@
-module.exports = () => {
+module.exports = (Discord, client) => {
   console.log('TheCarbonBot is online.');
+  client.user.setPresence({ activity: { name: `$help`, type: 'LISTENING' }, status: "online"});
 }
