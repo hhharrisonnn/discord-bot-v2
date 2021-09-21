b4d01718b8f027ab9aab7a2ea292dab5b418a186
hhharrisonnn <harrison.ho19@gmail.com>
2021-09-21 17:17:25 +0100

    Added automatic status changes

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,4 +1,23 @@
+//require('dotenv').config();
+const prefix = process.env.PREFIX;
+
 module.exports = (Discord, client) => {
   console.log(`${client.user.username} is online.`);
-  client.user.setPresence({ activity: { name: `$help`, type: 'LISTENING' }, status: "online"});
+
+  const arrayOfStatus = [
+    `Hi :)`,
+    `${prefix}help`,
+    `${client.guilds.cache.size} servers`,
+    `${client.channels.cache.size} channels`,
+    `${client.users.cache.size} users`,
+    `${client.user.tag}`,
+  ]
+
+  let index = 0;
+  setInterval(() => {
+    if (index === arrayOfStatus.length) index = 0;
+    const status = arrayOfStatus[index];
+    client.user.setActivity(status);
+    index++;
+  }, 10000);
 }
