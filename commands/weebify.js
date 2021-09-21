12ba1e9f8072bdb952a799fbf9caf6f58c3eaedd
ArrowPC <69222225+ArrowPC@users.noreply.github.com>
2021-09-22 05:21:11 +1000

    Added weebify,js

---
+++ commands/weebify.js
@@ -0,0 +1,45 @@
+module.exports = {
+  name: 'weebify',
+  permissions: [],
+  cooldown: 0,
+  aliases: ['uwu', 'uwufy', 'owo'],
+  description: 'uWu ur message >.<',
+  
+  async execute(message, args, cmd, client, Discord, profileData) {
+    if(!args.length) return message.reply('Please enter a message to uWufy >.<.');
+
+    faces = [
+        '(・`ω´・)',
+        ';;w;;',
+         'owo',
+        'UwU',
+         '>w<',
+         '^w^',
+        '( =①ω①=)',
+        '=＾● ⋏ ●＾=',
+        '（ΦωΦ）',
+        '⁽⁽ଘ( ˊωˋ )ଓ⁾⁾ ',
+        '– ̗̀ (ᵕ꒳ᵕ) ̖́ –',
+        '(⁄˘⁄ ⁄ ω⁄ ⁄ ˘⁄)♡',
+        ' (U ᵕ U❁)',
+        ' ( ´ω` )۶'
+            ]
+
+    let msg = args.join(' ');
+    let uwuMessage = msg
+    .replace(/(?:r|l)/g, 'w')
+    .replace(/(?:R|L)/g, 'W')
+    .replace(/(n)([aeiou])/gi, '$1y$2')
+    .replace(/ove/g, 'uv')
+    .replace(/th/g, 'ff')
+    .replace(/\!+/g, ' ' + faces[Math.floor(Math.random() * faces.length)] + ' ')
+
+    
+        message.channel.send(uwuMessage);
+      
+    }
+  
+  }
+
+
+
