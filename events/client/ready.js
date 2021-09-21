812777d6dd5887f5ad6512ec1c13cac3c6f0750c
hhharrisonnn <harrison.ho19@gmail.com>
2021-09-21 16:56:20 +0100

    Replaced bot name with client.user.username

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,4 +1,4 @@
 module.exports = (Discord, client) => {
-  console.log('TheCarbonBot is online.');
+  console.log(`${client.user.username} is online.`);
   client.user.setPresence({ activity: { name: `$help`, type: 'LISTENING' }, status: "online"});
 }
