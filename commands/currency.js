099e79bd9b35cc83fdf1284051e19a4ef0a1e28d
ArrowPC <69222225+ArrowPC@users.noreply.github.com>
2021-09-22 15:08:58 +1000

    updated currency.js
    now uses a better api, requires api key.

--- commands/currency.js
+++ commands/currency.js
@@ -2,38 +2,36 @@ const fetch = require('node-fetch');
 module.exports = {
     name: 'currency',
     permissions: [],
-    cooldown: 5,
-    aliases: ['convert'],
-    description: '',
+    cooldown: 300,
+    aliases: ['convert', 'exchangerate', 'er'],
+    description: 'converation rate between 2 currencies',
     
     async execute(message, args, cmd, client, Discord, profileData) {
-
+        const currency1 =  args.slice(1, 2).toString().toUpperCase('')
+        const amount = args[0].toUpperCase('')
         try {
             
-          if (args.includes('to')) {
+          if (args.includes('to') || args.includes('=>')) {
             
             const currency2 = args.slice(3).toString().toUpperCase('')
-            const currency1 =  args.slice(1, 2).toString().toUpperCase('')
-            const amount = args[0].toUpperCase('')
-            const host = `https://api.frankfurter.app/latest?amount=${amount}&from=${currency1}&to=${currency2}`
+            const host = `https://v6.exchangerate-api.com/v6/${process.env.EXCHANGERATE_KEY}/pair/${currency1}/${currency2}/${amount}`
             fetch(`${host}`)
             .then(resp => resp.json())
         .then((data) => {
-            message.reply(`${amount} ${currency1} = ${Object.values(data.rates)[0]} ${currency2}`);
+            message.reply(`${amount} ${currency1} = ${data.conversion_result} ${currency2}`);
           })
         }
         
 
           else {
             const currency2 = args.slice(2).toString().toUpperCase('')
-            const currency1 =  args.slice(1, 2).toString().toUpperCase('')
-            const amount = args[0].toUpperCase('')
-            const host = `https://api.frankfurter.app/latest?amount=${amount}&from=${currency1}&to=${currency2}`
+            const host = `https://v6.exchangerate-api.com/v6/${process.env.EXCHANGERATE_KEY}/pair/${currency1}/${currency2}/${amount}`
             fetch(`${host}`)
              .then(resp => resp.json())
         .then((data) => {
             console.log(data)
-            message.reply(`${amount} ${currency1} = ${Object.values(data.rates)[0]} ${currency2}`);
+            message.reply(`${amount} ${currency1} = ${data.conversion_result} ${currency2}`);
+            
         
         })
     }
@@ -41,6 +39,7 @@ module.exports = {
           catch (err) {
              message.reply(`make sure to use the correct format: !currency amount currency1 currency2`);
           }}
+          
         }
         
 
