f0d11408d1d1ef262dfab583f8f4e314ba3067b2
ArrowPC <69222225+ArrowPC@users.noreply.github.com>
2021-09-23 06:26:27 +1000

    added ability to check another user's balance

--- commands/balance.js
+++ commands/balance.js
@@ -4,16 +4,27 @@ module.exports = {
   permissions: [],
   cooldown: 0,
   description: 'Check balance.',
-  execute(message, args, cmd, client, Discord, profileData) {
-    
+  async execute(message, args, cmd, client, Discord, mentionData, profileData) {
+    if (message.mentions.members.first()) { 
   const newEmbed = new Discord.MessageEmbed()
+  .setColor('#304281')
+  .setTitle(':moneybag:')
+  .addFields(
+    {name: 'Balance:', value: `${mentionData.coins}`},
+    {name: 'Bank:', value: `${mentionData.bank}`},
+  ) 
+
+  message.channel.send(newEmbed)
+  }
+   else { 
+    const newEmbed = new Discord.MessageEmbed()
   .setColor('#304281')
   .setTitle(':moneybag:')
   .addFields(
     {name: 'Balance:', value: `${profileData.coins}`},
     {name: 'Bank:', value: `${profileData.bank}`},
   ) 
-
   message.channel.send(newEmbed)
-  }
+  } 
 }
+};
