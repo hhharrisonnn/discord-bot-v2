d5431066fdb644c7fdb0a80280564f1388c801be
ArrowPC <69222225+ArrowPC@users.noreply.github.com>
2021-09-23 12:38:26 +1000

    fixed deposit.js causing crash

--- commands/deposit.js
+++ commands/deposit.js
@@ -4,19 +4,25 @@ module.exports = {
   name: 'deposit',
   permissions: [],
   aliases: ['dep', 'd'],
-  cooldown: 86400,
+  cooldown: 0,
   description: 'Deposit coins into bank.',
   async execute(message, args, cmd, client, Discord, profileData) {
     let amount = args[0];
-    if(amount.toLowerCase() == "all") {
-      amount = profileData.coins
+    if (amount == "all") {
+      amount = profileData.coins;
+    }
+    if (!args.slice(1)) {
+      message.reply('please enter a valid amount of coins to deposit \<:FailFish:890034774796812288> ');
+    }
+    if (amount == 0) {
+      return message.reply(`you can't deposit 0 coins \<:FailFish:890034774796812288>`)
     }
     if(amount % 1 != 0 || amount <= 0) {
-      return message.channel.send('Please enter a valid amount of coins to deposit.')
+      return message.reply('please enter a valid amount of coins to deposit \<:FailFish:890034774796812288>')
     } 
 
     try {
-      if(amount > profileData.coins) return message.channel.send(`You don't have that amount of coins to deposit.`);
+      if(amount > profileData.coins) return message.reply(`you don't have that amount of coins to deposit \<:FailFish:890034774796812288>`);
       await profileModel.findOneAndUpdate({
         userID: message.author.id
       }, {
@@ -26,7 +32,15 @@ module.exports = {
         },
       });
 
-      return message.channel.send(`You deposited *${amount}* coins into your bank.`)
+       message.reply(`you deposited *${amount}* coins into your pockets.`)
+      const newEmbed = new Discord.MessageEmbed()
+      .setColor('#304281')
+      .setTitle(':moneybag:')
+      .addFields(
+        {name: 'Balance:', value: `${profileData.coins}`},
+        {name: 'Bank:', value: `${profileData.bank}`},
+      ) 
+      message.channel.send(newEmbed)
     }catch(err) {
       console.log(err)
     }
