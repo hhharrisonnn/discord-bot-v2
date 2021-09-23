9a882cf8cb099ce27106b33b22748dd80c8d0fcb
ArrowPC <69222225+ArrowPC@users.noreply.github.com>
2021-09-23 12:55:21 +1000

    added simple embeds for subage.js

--- commands/subage.js
+++ commands/subage.js
@@ -1,6 +1,7 @@
 const axios = require('axios'); 
 const sa = require("./subage.js",);
 const humanize = require('humanize-duration');
+const { User } = require('discord.js');
 module.exports = {
     name: 'subage',
     permissions: [],
@@ -43,10 +44,32 @@ module.exports = {
              if (subscriberCheck.data["subscribed"] == false) {
                  let oldsub = subscriberCheck.data["cumulative"];
                  if (oldsub["months"] === 0) {
-                     message.reply( `${user} has never been subscribed to ${broadcaster} :| .`)
+                     const embed = new Discord.MessageEmbed()
+                .setColor('#FF0000')
+                .setTitle(`${user} has never been subscribed to ${broadcaster} :|`)
+                .setAuthor(
+                  'Twitch',
+                 'https://i.nuuls.com/Om2N3.png',
+                 'https://twitch.tv/'
+                  )
+              .setURL(`https://api.ivr.fi/twitch/subage/${user}/${broadcaster}`)
+              .setTimestamp()
+                .setFooter('Powered by api.ivr.fi', '');
+             message.channel.send(embed);
                  }
                  else {
-                    message.reply( `${user} is not subscribed to ${broadcaster}, But has been previously for a total of ${oldsub["months"]} months!`);
+                    const embed = new Discord.MessageEmbed()
+                .setColor('#FFA500')
+                .setTitle(`${user} is not Subscribed to ${broadcaster}, But has been Previously for a Total of ${oldsub["months"]} months!`)
+                .setAuthor(
+                  'Twitch',
+                 'https://i.nuuls.com/Om2N3.png',
+                 'https://twitch.tv/'
+                  )
+                .setURL(`https://api.ivr.fi/twitch/subage/${user}/${broadcaster}`)
+                .setTimestamp()
+                .setFooter('Powered by api.ivr.fi', '');
+             message.channel.send(embed);
                  }
              }
              else {
@@ -56,14 +79,52 @@ module.exports = {
                  const ms = new Date().getTime() - Date.parse(subscriberData["endsAt"]);
      
                  if (subscriberData["type"] === "prime") {
-                     client.say( `${user} is currently subscribed to ${broadcaster} ﾠwith a tier 1 prime sub for a total of ${subscriberLength["months"]} months! They are currently on a ${subscriberStreak["months"]} months streak. The sub ends/renews in ${sa.humanizeDuration(ms)}.`);
+
+                    const embed = new Discord.MessageEmbed()
+                    .setColor('#00FF00')
+                    .setTitle(`**${user}** is currently subscribed to **${broadcaster}**ﾠwith a tier 1 prime sub for a total of **${subscriberLength["months"]} months!** They are currently on a **${subscriberStreak["months"]}** months streak. The sub ends/renews in **${sa.humanizeDuration(ms)}**.`)
+                    .setAuthor(
+                      'Twitch',
+                     'https://i.nuuls.com/Om2N3.png',
+                     'https://twitch.tv/'
+                      )
+                    .setURL(`https://api.ivr.fi/twitch/subage/${user}/${broadcaster}`)
+                    .setTimestamp()
+                    .setFooter('Powered by api.ivr.fi', '');
+                      message.channel.send(embed);
+
                  }
                  if (subscriberData["type"] === "paid") {
-                    message.reply( `${user} is currently subscribed to ${broadcaster} ﾠwith a tier ${subscriberData["tier"]} sub for a total of ${subscriberLength["months"]} months! They are currently on a ${subscriberStreak["months"]} months streak. The sub ends/renews in ${sa.humanizeDuration(ms)}.`);
+
+                    const embed = new Discord.MessageEmbed()
+                    .setColor('#00FF00')
+                    .setTitle(`**${user}** is currently subscribed to **${broadcaster}**ﾠwith a tier ${subscriberData["tier"]} sub for a total of **${subscriberLength["months"]} months!** They are currently on a ${subscriberStreak["months"]} months streak. The sub ends/renews in **${sa.humanizeDuration(ms)}**.`)
+                    .setAuthor(
+                      'Twitch',
+                     'https://i.nuuls.com/Om2N3.png',
+                     'https://twitch.tv/'
+                      )
+                    .setURL(`https://api.ivr.fi/twitch/subage/${user}/${broadcaster}`)
+                    .setTimestamp()
+                    .setFooter('Powered by api.ivr.fi', '');
+                      message.channel.send(embed);
+            
                  }
                  if (subscriberData["type"] === "gift") {
                      let gifter = subscriberData["gift"]["name"];
-                     message.reply( `${user} is currently subscribed to ${broadcaster}ﾠ with a tier ${subscriberData["tier"]} sub, gifted by ${gifter} for a total of ${subscriberLength["months"]} months! They are currently on a ${subscriberStreak["months"]} months streak. The sub ends/renews in ${sa.humanizeDuration(ms)}.`);
+                     const embed = new Discord.MessageEmbed()
+                    .setColor('#00FF00')
+                    .setTitle(`**${user}** is currently subscribed to **${broadcaster}**ﾠwith a tier ${subscriberData["tier"]} sub, gifted by **${gifter}** for a total of **${subscriberLength["months"]} months!** They are currently on a ${subscriberStreak["months"]} months streak. The sub ends/renews in **${sa.humanizeDuration(ms)}**.`)
+                    .setAuthor(
+                      'Twitch',
+                     'https://i.nuuls.com/Om2N3.png',
+                     'https://twitch.tv/'
+                      )
+                    .setURL(`https://api.ivr.fi/twitch/subage/${user}/${broadcaster}`)
+                    .setTimestamp()
+                    .setFooter('Powered by api.ivr.fi', '');
+                      message.channel.send(embed);
+    
                  }
              }
           }
