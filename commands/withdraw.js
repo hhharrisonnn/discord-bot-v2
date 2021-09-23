ca0cff4cfbbe17e558574c249b0a211fc919169f
ArrowPC <69222225+ArrowPC@users.noreply.github.com>
2021-09-23 12:38:58 +1000

    fixed withdraw.js causing crash

--- commands/withdraw.js
+++ commands/withdraw.js
@@ -1,3 +1,4 @@
+const { MessageReaction } = require("discord.js");
 const profileModel = require("../models/profileSchema");
 
 module.exports = {
@@ -8,15 +9,21 @@ module.exports = {
   description: 'Withdraw coins from your bank.',
   async execute(message, args, cmd, client, Discord, profileData) {
     let amount = args[0];
-    if(amount.toLowerCase() == "all") {
-      amount = profileData.bank
+    if (amount == "all") {
+      amount = profileData.bank;
+    }
+    if (amount == 0) {
+      return message.reply(`you can't withdraw 0 coins \<:FailFish:890034774796812288>`)
+    }
+    if (!args.slice('')) {
+      message.reply('please enter a valid amount of coins to withdraw \<:FailFish:890034774796812288> ');
     }
     if(amount % 1 != 0 || amount <= 0) {
-      return message.channel.send('Please enter a valid amount of coins to withdraw.')
-    }
+      return message.reply('Please enter a valid amount of coins to withrdraw  \<:FailFish:890034774796812288>')
+    } 
     
     try {
-      if(amount > profileData.bank) return message.channel.send(`You don't have that amount of coins to withdraw.`);
+      if(amount > profileData.bank) return message.reply(`you don't have that amount of coins to withdraw  \<:FailFish:890034774796812288>`);
       await profileModel.findOneAndUpdate({
         userID: message.author.id
       }, {
@@ -26,8 +33,15 @@ module.exports = {
         },
       });
   
-      return message.channel.send(`You withdrew *${amount}* coins from your bank.`)
-
+       message.reply(`you withdrew *${amount}* coins from your bank.`)
+      const newEmbed = new Discord.MessageEmbed()
+      .setColor('#304281')
+      .setTitle(':moneybag:')
+      .addFields(
+        {name: 'Balance:', value: `${profileData.coins}`},
+        {name: 'Bank:', value: `${profileData.bank}`},
+      ) 
+      message.channel.send(newEmbed)
     }catch(err) {
       console.log(err)
     }
