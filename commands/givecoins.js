a0c15dec5fb294a4fee560d8754ef62366f52677
ArrowPC <69222225+ArrowPC@users.noreply.github.com>
2021-09-23 12:35:32 +1000

    fixed givecoins.js causing crash

--- commands/givecoins.js
+++ commands/givecoins.js
@@ -6,22 +6,25 @@ module.exports = {
   permissions: [],
   cooldown: 0,
   description: 'Give coins to someone.',
-  async execute(message, args, cmd, client, Discord, profileData) {
+  async execute(message, args, cmd, client, Discord, profileData, mentionData) {
     let member = message.mentions.members.first();
     let amount = args[1];
-
-    if (!args[1]) {
-      message.reply('please enter a valid amount of coins to give.');
-    }
-    if (amount.toLowerCase() == "all") {
+        if (amount == "all") {
       amount = profileData.coins;
     }
+
+   if (amount == 0) {
+     return message.reply(`you can't give away 0 coins \<:FailFish:890034774796812288>`)
+   }
+    if (!args.slice(1)) {
+      message.reply('please enter a valid amount of coins to give \<:FailFish:890034774796812288> ');
+    }
     if (amount % 1 != 0 || amount <= 0) {
-      return message.reply('please enter a valid amount of coins to give.');
+      return message.reply('please enter a valid amount of coins to give \<:FailFish:890034774796812288>');
     }
     
     try {
-      if (amount > profileData.coins) return message.channel.send(`You don't have that amount of coins to give.`);
+      if (amount > profileData.coins || amount == 0) return message.reply(`you don't have that amount of coins to give \<:FailFish:890034774796812288>`);
       await profileModel.findOneAndUpdate({
         userID: message.author.id
       }, {
