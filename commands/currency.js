d5c450c9c19cc0a982f46dff27aa18dc217f5b11
ArrowPC <69222225+ArrowPC@users.noreply.github.com>
2021-10-02 19:08:52 +1000

    cleaned up code

--- commands/currency.js
+++ commands/currency.js
@@ -27,12 +27,13 @@ module.exports = {
               amount = parseFloat(appl) * 1;
             }
           }
-        
+        var currency2 = args.slice(2).toString().toUpperCase('')
         try {
             
           if (args.includes('to') || args.includes('=>')) {
             
-            const currency2 = args.slice(3).toString().toUpperCase('')
+             currency2 = args.slice(3).toString().toUpperCase('')
+          }
             const host = `https://v6.exchangerate-api.com/v6/${process.env.EXCHANGERATE_KEY}/pair/${currency1}/${currency2}/${amount}`
             fetch(`${host}`)
             .then(resp => resp.json())
@@ -48,29 +49,9 @@ module.exports = {
         
         message.channel.send(embed);
           })
-        }
         
-
-          else {
-            const currency2 = args.slice(2).toString().toUpperCase('')
-            const host = `https://v6.exchangerate-api.com/v6/${process.env.EXCHANGERATE_KEY}/pair/${currency1}/${currency2}/${amount}`
-            fetch(`${host}`)
-             .then(resp => resp.json())
-        .then((data) => {
-            const embed = new Discord.MessageEmbed()
-          .setColor('#FEC47F')
-          .setTitle(`${amount} ${currency1} = ${data.conversion_result} ${currency2}`)
-          .setAuthor(
-            'ExchangeRate',
-            'https://i.nuuls.com/d6PLz.png',
-            'https://www.exchangerate-api.com/'
-          )
-        
-        message.channel.send(embed);
-        })
-    }
-}
-          catch (err) {
+      } 
+       catch (err) {
              message.reply(`make sure to use the correct format: !currency amount currency1 currency2`);
           }}
           
