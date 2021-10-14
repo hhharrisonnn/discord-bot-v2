ee158d4d07a869f1111ac3f59a0b731bc1ffffd4
hhharrisonnn <harrison.ho19@gmail.com>
2021-10-14 18:45:30 +0100

    Change to types of status activity

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,22 +1,22 @@
-const prefix = process.env.PREFIX;
-
 module.exports = (Discord, client) => {
   console.log(`${client.user.username} is online.`);
 
   const arrayOfStatus = [
-    `Hi :)`,
-    `${prefix}help`,
+    `${process.env.PREFIX}help`,
     `${client.guilds.cache.size} servers`,
-    `${client.channels.cache.size} channels`,
     `${client.users.cache.size} users`,
-    `${client.user.tag}`,
-  ]
+  ];
 
   let index = 0;
   setInterval(() => {
     if (index === arrayOfStatus.length) index = 0;
     const status = arrayOfStatus[index];
-    client.user.setActivity(status);
+    if (status === arrayOfStatus[0]) {
+      var typeOfActivity = 'PLAYING';
+    } else {
+      var typeOfActivity = 'WATCHING';
+    }
+    client.user.setActivity(status, { type: `${typeOfActivity}` });
     index++;
   }, 10000);
 }
