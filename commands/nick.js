fcc17a4a120319370de7669b46b5e6f6a56f977c
ArrowPC <69222225+ArrowPC@users.noreply.github.com>
2021-10-21 06:37:32 +1100

    Update nick.js

--- commands/nick.js
+++ commands/nick.js
@@ -1,20 +1,22 @@
-module.exports = {
-  name: 'nick',
-  permissions: ["MANAGE_NICKNAMES"],
-  cooldown: 0,
-  aliases: ['nickname'],
-  description: 'Change someone\'s nickname.',
-  execute(message, args, cmd, client, Discord, profileData) {
-    const member = message.mentions.members.first() || message.member;
-
-    if (!member) return message.reply('Please specify a member.');
-
-    const argument = args.slice(1).join(' ');
-
-    try {
-      member.setNickname(argument);
-    } catch (err) {
-      message.reply('I do not have permission to set ' + member.toString() + ' nickname.');
-    }
-  }
-}
+module.exports = { 
+  name: 'nick', 
+  permissions: ['MANAGE_NICKNAMES'], 
+  cooldown: 0, 
+  aliases: ['nickname'], 
+  description: "Change someone's nickname.", 
+  execute(message, args, cmd, client, Discord, profileData) { 
+    const member = message.mentions.members.first() || message.member; 
+ 
+    if (!member) return message.reply('Please specify a member.'); 
+ 
+    const argument = args.slice(1).join(' '); 
+ 
+    try { 
+      member.setNickname(argument); 
+    } catch (err) { 
+      message.reply( 
+        'I do not have permission to set ' + member.toString() + ' nickname.' 
+      ); 
+    } 
+  }, 
+}; 
