eee17c372e172944c67708a4006b1668b5fc4f8e
ArrowPC <69222225+ArrowPC@users.noreply.github.com>
2021-10-21 06:09:14 +1100

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,21 +1,22 @@
 module.exports = {
   name: 'ban',
-  permissions: ["BAN_MEMBERS"],
+  permissions: ['BAN_MEMBERS'],
   cooldown: 0,
   description: 'This command bans a member.',
   async execute(message, args, cmd, client, Discord, profileData) {
     let banner = message.author;
     let member = message.mentions.members.first();
-    if (!member) return message.reply('you need to mention a member to ban them.');
+    if (!member)
+      return message.reply('you need to mention a member to ban them.');
     let reason = args.slice(1).join(' ');
     if (!reason) reason = '*No Reason Given*';
     if (!member.bannable) return message.reply('this member is not bannable');
 
     const embed = new Discord.MessageEmbed()
-    .setColor('RANDOM')
-    .addField('You have been banned from:', message.guild)
-    .addField('Reason:', reason)
-    .setThumbnail(message.guild.iconURL())
+      .setColor('RANDOM')
+      .addField('You have been banned from:', message.guild)
+      .addField('Reason:', reason)
+      .setThumbnail(message.guild.iconURL());
 
     const user = await client.users.fetch(member.id);
 
@@ -24,8 +25,10 @@ module.exports = {
     } catch {
       console.log(`Ban: Could not DM the user ${member}.`);
     } finally {
-      member.ban({reason: `Banned by: ${banner.tag} Banner ID: ${banner} | Reason: ${reason}`});
+      member.ban({
+        reason: `Banned by: ${banner.tag} Banner ID: ${banner} | Reason: ${reason}`,
+      });
       message.reply(`${member} ID: ${member.id} has been banned.`);
     }
-  }
-}
+  },
+};
