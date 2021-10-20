f804722108d3011ac1c168a19d4d3128b780997a
ArrowPC <69222225+ArrowPC@users.noreply.github.com>
2021-10-21 06:46:12 +1100

    Update subage.js

--- commands/subage.js
+++ commands/subage.js
@@ -1,5 +1,5 @@
-const axios = require('axios'); 
-const sa = require("./subage.js",);
+const axios = require('axios');
+const sa = require('./subage.js');
 const humanize = require('humanize-duration');
 
 module.exports = {
@@ -9,126 +9,167 @@ module.exports = {
   aliases: ['sa'],
   description: 'Check the Twitch subage of a user to a channel',
   async execute(message, args, cmd, client, Discord, profileData) {
-    const color = '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6);
+    const color =
+      '#' + (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6);
 
     try {
       exports.humanizeDuration = (ms) => {
         const options = {
-          language: "shortEn",
+          language: 'shortEn',
           languages: {
-          shortEn: {
-            y: () => "y",
-            mo: () => "mo",
-            w: () => "w",
-            d: () => "d",
-            h: () => "h",
-            m: () => "m",
-            s: () => "s",
-            ms: () => "ms",
-          },
+            shortEn: {
+              y: () => 'y',
+              mo: () => 'mo',
+              w: () => 'w',
+              d: () => 'd',
+              h: () => 'h',
+              m: () => 'm',
+              s: () => 's',
+              ms: () => 'ms',
+            },
           },
           units: ['y', 'd', 'h', 'm', 's'],
           largest: 3,
           round: true,
           conjunction: ' and ',
           spacer: '',
-        }
+        };
         return humanize(ms, options);
-      }
-            
-      const user =  args[0];
-      if (!args[0]) return message.reply('enter a user and a channel.')
+      };
+
+      const user = args[0];
+      if (!args[0]) return message.reply('enter a user and a channel.');
       const broadcaster = args.slice(1);
-     
-      let subscriberCheck = await axios.get(`https://api.ivr.fi/twitch/subage/${user}/${broadcaster}`);
-      if (subscriberCheck.data["subscribed"] == false) {
-        let oldsub = subscriberCheck.data["cumulative"];
-        if (oldsub["months"] === 0) {
+
+      let subscriberCheck = await axios.get(
+        `https://api.ivr.fi/twitch/subage/${user}/${broadcaster}`
+      );
+      if (subscriberCheck.data['subscribed'] == false) {
+        let oldsub = subscriberCheck.data['cumulative'];
+        if (oldsub['months'] === 0) {
           const embed = new Discord.MessageEmbed()
-          .setColor(color)
-          .setTitle(`${user} => ${broadcaster}`)
-          .setThumbnail('https://i.nuuls.com/Om2N3.png')
-          .addField(' ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎  ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‎‏‎‏‏‎‏‏‎Status', `Has never been subscribed`, true)
-          .setURL(`https://api.ivr.fi/twitch/subage/${user}/${broadcaster}`)
-          .setTimestamp()
-          .setFooter('Powered by api.ivr.fi', '');
+            .setColor(color)
+            .setTitle(`${user} => ${broadcaster}`)
+            .setThumbnail('https://i.nuuls.com/Om2N3.png')
+            .addField(
+              ' ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎  ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‎‏‎‏‏‎‏‏‎Status',
+              `Has never been subscribed`,
+              true
+            )
+            .setURL(`https://api.ivr.fi/twitch/subage/${user}/${broadcaster}`)
+            .setTimestamp()
+            .setFooter('Powered by api.ivr.fi', '');
+          message.channel.send(embed);
+        } else {
+          const embed = new Discord.MessageEmbed()
+            .setColor(color)
+            .setTitle(`${user} => ${broadcaster}`)
+            .setThumbnail('https://i.nuuls.com/Om2N3.png')
+            .addField(
+              ' ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‎‏‎‏‏‎‏‏‎Status',
+              `Not subbed`,
+              true
+            )
+            .addField(
+              'Previously subbed for',
+              `‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ${oldsub['months']} months`,
+              true
+            )
+            .setURL(`https://api.ivr.fi/twitch/subage/${user}/${broadcaster}`)
+            .setTimestamp()
+            .setFooter('Powered by api.ivr.fi', '');
           message.channel.send(embed);
         }
-        else {
+      } else {
+        let subscriberData = subscriberCheck.data['meta'];
+        let subscriberLength = subscriberCheck.data['cumulative'];
+        let subscriberStreak = subscriberCheck.data['streak'];
+        const ms = new Date().getTime() - Date.parse(subscriberData['endsAt']);
+
+        if (subscriberData['type'] === 'prime') {
           const embed = new Discord.MessageEmbed()
-          .setColor(color)
-          .setTitle(`${user} => ${broadcaster}`)
-          .setThumbnail('https://i.nuuls.com/Om2N3.png')
-          .addField(' ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‎‏‎‏‏‎‏‏‎Status', `Not subbed`, true)
-          .addField('Previously subbed for', `‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ${oldsub["months"]} months`, true)
-          .setURL(`https://api.ivr.fi/twitch/subage/${user}/${broadcaster}`)
-          .setTimestamp()
-          .setFooter('Powered by api.ivr.fi', '');
+            .setColor(color)
+            .setTitle(`${user} => ${broadcaster}`)
+            .setThumbnail('https://i.nuuls.com/Om2N3.png')
+            .addField('Status', `Subbed`, true)
+            .addField('Type', `Prime`, true)
+            .addField('Total', `${subscriberLength['months']} months`, true)
+            .addField(
+              ' ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎  ‎‏‏‎ ‎‏‏‎‎‏‏‎ ‎‏‏‎Tier',
+              ` ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‏‏‎ ‎ ‎‏‏‎ ‎‏‏‎  ‎‏‏${subscriberData['tier']}`,
+              true
+            )
+            .addField('Streak', `${subscriberStreak['months']} months`, true)
+            .addField(
+              ' ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ Renews/Ends in',
+              ` ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ${sa.humanizeDuration(
+                ms
+              )}`
+            )
+            .setURL(`https://api.ivr.fi/twitch/subage/${user}/${broadcaster}`)
+            .setTimestamp()
+            .setFooter('Powered by api.ivr.fi', '');
+          message.channel.send(embed);
+        }
+
+        if (subscriberData['type'] === 'paid') {
+          const embed = new Discord.MessageEmbed()
+            .setColor(color)
+            .setTitle(`${user} => ${broadcaster}`)
+            .setThumbnail('https://i.nuuls.com/Om2N3.png')
+            .addField('Status', `Subbed`, true)
+            .addField('Type', `Paid`, true)
+            .addField('Total', `${subscriberLength['months']} months`, true)
+            .addField(
+              ' ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎  ‎‏‏‎ ‎‏‏‎‎‏‏‎ ‎‏‏‎Tier',
+              ` ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‏‏‎ ‎ ‎‏‏‎ ‎‏‏‎  ‎‏‏${subscriberData['tier']}`,
+              true
+            )
+            .addField('Streak', `${subscriberStreak['months']} months`, true)
+            .addField(
+              ' ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ Renews/Ends in',
+              ` ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ${sa.humanizeDuration(
+                ms
+              )}`
+            )
+            .setURL(`https://api.ivr.fi/twitch/subage/${user}/${broadcaster}`)
+            .setTimestamp()
+            .setFooter('Powered by api.ivr.fi', '');
+          message.channel.send(embed);
+        }
+
+        if (subscriberData['type'] === 'gift') {
+          let gifter = subscriberData['gift']['name'];
+          const embed = new Discord.MessageEmbed()
+            .setColor(color)
+            .setTitle(`${user} => ${broadcaster}`)
+            .setThumbnail('https://i.nuuls.com/Om2N3.png')
+            .addField('Status', `Subbed`, true)
+            .addField('Type', `Gifted`, true)
+            .addField('Total', `${subscriberLength['months']} months`, true)
+            .addField(
+              ' ‎‏‏‎ ‎ ‎‏‏‎‎‏‏‎Tier',
+              ` ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏${subscriberData['tier']}`,
+              true
+            )
+            .addField('Gifter', `${gifter}`, true)
+            .addField('Streak', `${subscriberStreak['months']} months`, true)
+            .addField(
+              ' ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎‎‏‏‎ ‎‏‏‎‏‏‎‎‏‏‎Renews/Ends in',
+              ` ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎‎‏‏‎ ‎‏‏‎‎‏‏‎‎‏‏‎ ‎‏‏ ‎‏${sa.humanizeDuration(
+                ms
+              )}`
+            )
+            .setURL(`https://api.ivr.fi/twitch/subage/${user}/${broadcaster}`)
+            .setTimestamp()
+            .setFooter('Powered by api.ivr.fi', '');
           message.channel.send(embed);
         }
       }
-      else {
-        let subscriberData = subscriberCheck.data["meta"];
-        let subscriberLength = subscriberCheck.data["cumulative"];
-        let subscriberStreak = subscriberCheck.data["streak"]; 
-        const ms = new Date().getTime() - Date.parse(subscriberData["endsAt"]);
-   
-        if (subscriberData["type"] === "prime") {
-          const embed = new Discord.MessageEmbed()
-          .setColor(color)
-          .setTitle(`${user} => ${broadcaster}`)
-          .setThumbnail('https://i.nuuls.com/Om2N3.png')
-          .addField('Status', `Subbed`, true)
-          .addField('Type', `Prime`, true)
-          .addField('Total', `${subscriberLength["months"]} months`, true)
-          .addField(' ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎  ‎‏‏‎ ‎‏‏‎‎‏‏‎ ‎‏‏‎Tier', ` ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‏‏‎ ‎ ‎‏‏‎ ‎‏‏‎  ‎‏‏${subscriberData["tier"]}`, true)
-          .addField('Streak', `${subscriberStreak["months"]} months`, true)
-          .addField(' ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ Renews/Ends in', ` ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ${sa.humanizeDuration(ms)}`)
-          .setURL(`https://api.ivr.fi/twitch/subage/${user}/${broadcaster}`)
-          .setTimestamp()
-          .setFooter('Powered by api.ivr.fi', '');
-          message.channel.send(embed);
-        }
-          
-        if (subscriberData["type"] === "paid") {
-          const embed = new Discord.MessageEmbed()
-          .setColor(color)
-          .setTitle(`${user} => ${broadcaster}`)
-          .setThumbnail('https://i.nuuls.com/Om2N3.png')
-          .addField('Status', `Subbed`, true)
-          .addField('Type', `Paid`, true)
-          .addField('Total', `${subscriberLength["months"]} months`, true)
-          .addField(' ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎  ‎‏‏‎ ‎‏‏‎‎‏‏‎ ‎‏‏‎Tier', ` ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‏‏‎ ‎ ‎‏‏‎ ‎‏‏‎  ‎‏‏${subscriberData["tier"]}`, true)
-          .addField('Streak', `${subscriberStreak["months"]} months`, true)
-          .addField(' ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ Renews/Ends in', ` ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ${sa.humanizeDuration(ms)}`)
-          .setURL(`https://api.ivr.fi/twitch/subage/${user}/${broadcaster}`)
-          .setTimestamp()
-          .setFooter('Powered by api.ivr.fi', '');
-          message.channel.send(embed);
-        }
-                 
-        if (subscriberData["type"] === "gift") {
-        let gifter = subscriberData["gift"]["name"];
-        const embed = new Discord.MessageEmbed()
-        .setColor(color)
-        .setTitle(`${user} => ${broadcaster}`)
-        .setThumbnail('https://i.nuuls.com/Om2N3.png')
-        .addField('Status', `Subbed`, true)
-        .addField('Type', `Gifted`, true)
-        .addField('Total', `${subscriberLength["months"]} months`, true)
-        .addField(' ‎‏‏‎ ‎ ‎‏‏‎‎‏‏‎Tier', ` ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏${subscriberData["tier"]}`, true)
-        .addField('Gifter', `${gifter}`, true)
-        .addField('Streak', `${subscriberStreak["months"]} months`, true)
-        .addField(' ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎‎‏‏‎ ‎‏‏‎‏‏‎‎‏‏‎Renews/Ends in', ` ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎‎‏‏‎ ‎‏‏‎‎‏‏‎‎‏‏‎ ‎‏‏ ‎‏${sa.humanizeDuration(ms)}`)
-        .setURL(`https://api.ivr.fi/twitch/subage/${user}/${broadcaster}`)
-        .setTimestamp()
-        .setFooter('Powered by api.ivr.fi', '');
-        message.channel.send(embed);
-        }
-      }
+    } catch (err) {
+      message.reply(
+        `make sure to use the correct format: ${process.env.PREFIX}subage, sa [user] [channel].`
+      );
     }
-    catch (err) {
-      message.reply(`make sure to use the correct format: ${process.env.PREFIX}subage, sa [user] [channel].`);
-    }
-  }
-}
+  },
+};
