04c1ab105f85f2e09953baa6eb381b434b4008a8
ArrowPC <69222225+ArrowPC@users.noreply.github.com>
2021-10-21 06:26:22 +1100

    Update emojify.js

--- commands/emojify.js
+++ commands/emojify.js
@@ -7,34 +7,40 @@ module.exports = {
   aliases: [''],
   description: 'Emojify a message',
   async execute(message, args, cmd, client, Discord, profileData) {
-    if(!args.length) return message.reply('please enter a message to emojify.');
+    if (!args.length)
+      return message.reply('Please enter a message to emojify.');
     const specialCodes = {
-      '0': ':zero:',
-      '1': ':one:',
-      '2': ':two:',
-      '3': ':three:',
-      '4': ':four:',
-      '5': ':five:',
-      '6': ':six:',
-      '7': ':seven:',
-      '8': ':eight:',
-      '9': ':nine:',
+      0: ':zero:',
+      1: ':one:',
+      2: ':two:',
+      3: ':three:',
+      4: ':four:',
+      5: ':five:',
+      6: ':six:',
+      7: ':seven:',
+      8: ':eight:',
+      9: ':nine:',
       '#': ':hash:',
       '*': ':asterisk:',
       '?': ':grey_question:',
       '!': ':grey_exclamation:',
-      ' ': '   '
-    }
+      ' ': '   ',
+    };
 
-    const text = args.join(" ").toLowerCase().split('').map(letter => {
-      if(/[a-z]/g.test(letter)) {
-        return `:regional_indicator_${letter}:`
-      } else if (specialCodes[letter]) {
-        return `${specialCodes[letter]}`
-      }
-      return letter;
-    }).join('');
+    const text = args
+      .join(' ')
+      .toLowerCase()
+      .split('')
+      .map((letter) => {
+        if (/[a-z]/g.test(letter)) {
+          return `:regional_indicator_${letter}:`;
+        } else if (specialCodes[letter]) {
+          return `${specialCodes[letter]}`;
+        }
+        return letter;
+      })
+      .join('');
 
     message.channel.send(text);
-  }
-}
+  },
+};
