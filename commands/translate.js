2ad31034b8cf292de5adecf7be891a8808f995dd
ArrowPC <69222225+ArrowPC@users.noreply.github.com>
2021-10-21 06:50:45 +1100

    Update translate.js

--- commands/translate.js
+++ commands/translate.js
@@ -8,78 +8,99 @@ module.exports = {
   description: 'Translate something into English.',
   execute(message, args, cmd, client, Discord, profileData) {
     if (args.includes('to')) {
-      const language =  args.join(" ").split(" ").pop();
+      const language = args.join(' ').split(' ').pop();
       var word = ` to *`;
       var pat = new RegExp('(\\b' + word + '\\b)(?!.*\\b\\1\\b)', 'is');
-      text = args.join(' ').split(pat)[0]
-      translate(text, {to : `${language}`}).then(res => {
-        const embed = new Discord.MessageEmbed()
-        .setColor('RANDOM')
-        .addField(`   Original`,`${text}`, true)
-        .addField(`${language.replace(/^./, language[0].toUpperCase())} translation`, `${res}`, true)
-        .setAuthor(
-          'Google Translate')
-        .setThumbnail(`https://i.nuuls.com/uQMal.png`)
-        .setTimestamp()
-        .setFooter('Powered by Google-Translate', '')
-        
-        message.channel.send(embed).then(msg => {
-          let interval = setInterval(() => {
-            let newColor = '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6);
-            let embed2 = new Discord.MessageEmbed()
-            .setColor(newColor)
-            .addField(`   Original`,`${text}`, true)
-            .addField(`${language.replace(/^./, language[0].toUpperCase())} translation`, `${res}`, true)
-            .setAuthor(
-              'Google Translate')
+      text = args.join(' ').split(pat)[0];
+      translate(text, { to: `${language}` })
+        .then((res) => {
+          const embed = new Discord.MessageEmbed()
+            .setColor('RANDOM')
+            .addField(`   Original`, `${text}`, true)
+            .addField(
+              `${language.replace(
+                /^./,
+                language[0].toUpperCase()
+              )} translation`,
+              `${res}`,
+              true
+            )
+            .setAuthor('Google Translate')
             .setThumbnail(`https://i.nuuls.com/uQMal.png`)
             .setTimestamp()
-            .setFooter('Powered by Google-Translate', '')
-            msg.edit(embed2);
-          }, 5000);
-    
-          setTimeout(() => {
-            clearInterval(interval);
-          }, 60000);
+            .setFooter('Powered by Google-Translate', '');
+
+          message.channel.send(embed).then((msg) => {
+            let interval = setInterval(() => {
+              let newColor =
+                '#' +
+                (0x1000000 + Math.random() * 0xffffff)
+                  .toString(16)
+                  .substr(1, 6);
+              let embed2 = new Discord.MessageEmbed()
+                .setColor(newColor)
+                .addField(`   Original`, `${text}`, true)
+                .addField(
+                  `${language.replace(
+                    /^./,
+                    language[0].toUpperCase()
+                  )} translation`,
+                  `${res}`,
+                  true
+                )
+                .setAuthor('Google Translate')
+                .setThumbnail(`https://i.nuuls.com/uQMal.png`)
+                .setTimestamp()
+                .setFooter('Powered by Google-Translate', '');
+              msg.edit(embed2);
+            }, 5000);
+
+            setTimeout(() => {
+              clearInterval(interval);
+            }, 60000);
+          });
+        })
+        .catch((err) => {
+          console.log(err);
         });
-      }).catch(err => {
-        console.log(err);
-      });
-    }
-    else {
-      translate(args.join(' '), {to : ''}).then(res => {
-        const embed = new Discord.MessageEmbed()
-        .setColor('RANDOM')
-        .addField(` Original`,`${args.join(' ')}`, true)
-        .addField(`English translation`, `${res}`, true)
-        .setAuthor(
-          'Google Translate')
-        .setThumbnail(`https://i.nuuls.com/uQMal.png`)
-        .setTimestamp()
-        .setFooter('Powered by Google-Translate', '')
-        
-        message.channel.send(embed).then(msg => {
-          let interval = setInterval(() => {
-            let newColor = '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6);
-            let embed2 = new Discord.MessageEmbed()
-            .setColor(newColor)
-            .addField(` Original`,`${args.join(' ')}`, true)
+    } else {
+      translate(args.join(' '), { to: '' })
+        .then((res) => {
+          const embed = new Discord.MessageEmbed()
+            .setColor('RANDOM')
+            .addField(` Original`, `${args.join(' ')}`, true)
             .addField(`English translation`, `${res}`, true)
-            .setAuthor(
-              'Google Translate')
+            .setAuthor('Google Translate')
             .setThumbnail(`https://i.nuuls.com/uQMal.png`)
             .setTimestamp()
-            .setFooter('Powered by Google-Translate', '')
-            msg.edit(embed2);
-          }, 5000);
-    
-          setTimeout(() => {
-            clearInterval(interval);
-          }, 60000);
+            .setFooter('Powered by Google-Translate', '');
+
+          message.channel.send(embed).then((msg) => {
+            let interval = setInterval(() => {
+              let newColor =
+                '#' +
+                (0x1000000 + Math.random() * 0xffffff)
+                  .toString(16)
+                  .substr(1, 6);
+              let embed2 = new Discord.MessageEmbed()
+                .setColor(newColor)
+                .addField(` Original`, `${args.join(' ')}`, true)
+                .addField(`English translation`, `${res}`, true)
+                .setAuthor('Google Translate')
+                .setThumbnail(`https://i.nuuls.com/uQMal.png`)
+                .setTimestamp()
+                .setFooter('Powered by Google-Translate', '');
+              msg.edit(embed2);
+            }, 5000);
+
+            setTimeout(() => {
+              clearInterval(interval);
+            }, 60000);
+          });
+        })
+        .catch((err) => {
+          console.log(err);
         });
-      }).catch(err => {
-        console.log(err);
-      });
     }
-  }
-}
+  },
+};
