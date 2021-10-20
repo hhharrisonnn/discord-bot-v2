6f6686861125906d44979a1c2490ccdde6d75113
ArrowPC <69222225+ArrowPC@users.noreply.github.com>
2021-10-21 06:46:57 +1100

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,9 +1,10 @@
-module.exports = {
-  name: 'test',
-  permissions: [],
-  cooldown: 0,
-  description: 'This is a test command.',
-  execute(message, args) {
-    message.channel.send('Test fail lul');
-  }
-}
+module.exports = { 
+  name: 'test', 
+  permissions: [], 
+  cooldown: 0, 
+  description: 'This is a test command.', 
+  execute(message, args) { 
+    message.channel.send('Test fail lul'); 
+  }, 
+}; 
+
