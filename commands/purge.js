29b3643c8ce916b3c18510acaebbbf5a0004568c
ArrowPC <69222225+ArrowPC@users.noreply.github.com>
2021-10-21 06:42:12 +1100

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -1,15 +1,21 @@
 module.exports = {
   name: 'purge',
-  permissions: ["ADMINISTRATOR"],
+  permissions: ['ADMINISTRATOR'],
   cooldown: 0,
   description: 'Purge messages.',
+
   async execute(message, args, cmd, client, Discord) {
-    if (!args[0]) return message.reply('please enter the amount of messages to clear.');
+    if (!args[0])
+      return message.reply('please enter the amount of messages to clear.');
     if (isNaN(args[0])) return message.reply('please type a real number.');
-    if (args[0] > 100) return message.reply('you can\'t remove more than 100 messages.');
-    if (args[0] < 1) return message.reply('you have to delete at least one message.');
-    await message.channel.messages.fetch({ limit: args[0]}).then(messages =>{
-      message.channel.bulkDelete(messages);
-    });
-  }
-} 
+    if (args[0] > 100)
+      return message.reply("you can't remove more than 100 messages.");
+    if (args[0] < 1)
+      return message.reply('you have to delete at least one message.');
+    await message.channel.messages
+      .fetch({ limit: args[0] })
+      .then((messages) => {
+        message.channel.bulkDelete(messages);
+      });
+  },
+};
