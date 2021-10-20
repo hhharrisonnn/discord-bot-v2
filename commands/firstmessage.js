c8e64190dea6055420922afabae86961b62835d1
ArrowPC <69222225+ArrowPC@users.noreply.github.com>
2021-10-21 06:27:12 +1100

    Update firstmessage.js

--- commands/firstmessage.js
+++ commands/firstmessage.js
@@ -5,7 +5,6 @@ module.exports = {
   aliases: ['firstmsg', 'fl'],
   description: 'Sends the first message of a channel.',
   async execute(message, args, cmd, client, Discord, profileData) {
-    
     const fetchMessages = await message.channel.messages.fetch({
       after: 1,
       limit: 1,
@@ -16,40 +15,39 @@ module.exports = {
     if (msg.type == 'GUILD_MEMBER_JOIN') {
       message.channel.send(
         new Discord.MessageEmbed()
-        .setColor('RANDOM')
-        .setTitle(`First Message in #${message.channel.name}`)
-        .setThumbnail(msg.author.displayAvatarURL({ dynamic: true }))
-        .setDescription('*click link above to go to original message*')
-        .addField('User:', msg.author, true)
-        .addField('Joined at:', msg.createdAt.toLocaleDateString(), true)
-        .setURL(msg.url)
+          .setColor('RANDOM')
+          .setTitle(`First Message in #${message.channel.name}`)
+          .setThumbnail(msg.author.displayAvatarURL({ dynamic: true }))
+          .setDescription('*click link above to go to original message*')
+          .addField('User:', msg.author, true)
+          .addField('Joined at:', msg.createdAt.toLocaleDateString(), true)
+          .setURL(msg.url)
       );
     }
     if (msg.content) {
       message.channel.send(
         new Discord.MessageEmbed()
-        .setColor('RANDOM')
-        .setTitle(`First Message in #${message.channel.name}`)
-        .setURL(msg.url)
-        .setThumbnail(msg.author.displayAvatarURL({ dynamic: true }))
-        .setDescription('*click link above to go to original message*')
-        .addField('Author:', msg.author, true)
-        .addField('Message:', msg.content, true)
-        .addField('Created At:', msg.createdAt.toLocaleDateString(), true)
+          .setColor('RANDOM')
+          .setTitle(`First Message in #${message.channel.name}`)
+          .setURL(msg.url)
+          .setThumbnail(msg.author.displayAvatarURL({ dynamic: true }))
+          .setDescription('*click link above to go to original message*')
+          .addField('Author:', msg.author, true)
+          .addField('Message:', msg.content, true)
+          .addField('Created At:', msg.createdAt.toLocaleDateString(), true)
       );
-    }
-    else {
+    } else {
       message.channel.send(
         new Discord.MessageEmbed()
-        .setColor('RANDOM')
-        .setTitle(`First Message in #${message.channel.name}`)
-        .setURL(msg.url)
-        .setThumbnail(msg.author.displayAvatarURL({ dynamic: true }))
-        .setDescription('*click link above to go to original message*')
-        .addField('Author:', msg.author, true)
-        .addField('Embed Title:', msg.embeds[0].title, true)
-        .addField('Created At:', msg.createdAt.toLocaleDateString(), true)
+          .setColor('RANDOM')
+          .setTitle(`First Message in #${message.channel.name}`)
+          .setURL(msg.url)
+          .setThumbnail(msg.author.displayAvatarURL({ dynamic: true }))
+          .setDescription('*click link above to go to original message*')
+          .addField('Author:', msg.author, true)
+          .addField('Embed Title:', msg.embeds[0].title, true)
+          .addField('Created At:', msg.createdAt.toLocaleDateString(), true)
       );
-    }   
-  }
-}
+    }
+  },
+};
