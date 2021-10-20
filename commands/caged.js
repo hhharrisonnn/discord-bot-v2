7f8487daf95666464b3576c5982fd1bfb8b0763d
ArrowPC <69222225+ArrowPC@users.noreply.github.com>
2021-10-21 06:11:49 +1100

    Update caged.js

--- commands/caged.js
+++ commands/caged.js
@@ -1,10 +1,12 @@
 module.exports = {
   name: 'caged',
-  aliases: ['cage','cagedweebs'],
+  aliases: ['cage', 'cagedweebs'],
   permissions: [],
   cooldown: 0,
   description: 'Check how many weebs you have caged.',
   execute(message, args, cmd, client, Discord, profileData) {
-    message.reply(`you currently have ${profileData.huntweebs} weebs in the cage!`);
-  }
-}
+    message.channel.send(
+      `${message.author.username} you currently have ${profileData.huntweebs} weebs in the cage!`
+    );
+  },
+};
