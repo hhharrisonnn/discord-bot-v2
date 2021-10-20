463a561bfb30916a19b3c7a0f3cf67fc7a0f87f0
ArrowPC <69222225+ArrowPC@users.noreply.github.com>
2021-10-21 06:59:51 +1100

    Update index.js

--- index.js
+++ index.js
@@ -6,21 +6,21 @@ const mongoose = require('mongoose');
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
 
-['command_handler', 'event_handler'].forEach(handler => {
-  require(`./handlers/${handler}`) (client, Discord);
+['command_handler', 'event_handler'].forEach((handler) => {
+  require(`./handlers/${handler}`)(client, Discord);
 });
 
 mongoose
-.connect(process.env.MONGODB_SRV, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false
-})
-.then(() => {
-  console.log('Connected to the database.');
-})
-.catch((err) => {
-  console.log(err);
-});
+  .connect(process.env.MONGODB_SRV, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('Connected to the database.');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 client.login(process.env.DISCORD_TOKEN);
