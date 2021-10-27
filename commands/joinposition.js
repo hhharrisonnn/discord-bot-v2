703039bbe93c1883b6482b0f259aa9c5627cab8b
Oliver <oliver@archer.fi>
2021-10-27 22:03:22 +0100

    Removed redundant promise
    Removed redundant promise and made so the bot will default to returning
    the member who used the command if no other member is provided

--- commands/joinposition.js
+++ commands/joinposition.js
@@ -7,20 +7,15 @@ module.exports = {
   aliases: ['joinpos'],
   description: 'Check the join position of a member.',
   async execute(message, args, cmd, client, Discord, profileData) {
-    const member = message.mentions.members.first();
-
-    if (!member) return message.reply('please specify a member.');
+    let member = message.mentions.members.first();
+    member = member ? member : message.member;
 
     const members = message.guild.members.cache
       .sort((a, b) => a.joinedTimestamp - b.joinedTimestamp)
       .array();
 
-    const position = new Promise((ful) => {
-      for (let i = 1; i < members.length + 1; i++) {
-        if (members[i - 1].id === member.id) ful(i);
-      }
-    });
+    const position = members.findIndex(m => m.id === member.id) + 1;
 
-    message.reply(`${member} is number ${await position} to join the server.`);
+    message.reply(`${member} is number ${position} to join the server.`);
   },
 };
