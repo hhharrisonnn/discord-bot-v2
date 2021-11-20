f6ac78897245a1d9f0f3ba41a244c62e7b9daee6
hhharrisonnn <harrison.ho19@gmail.com>
2021-11-20 16:39:18 +0000

    Changes to format of profile command

--- commands/lastfm.js
+++ commands/lastfm.js
@@ -7,10 +7,8 @@ module.exports = {
   permissions: [],
   cooldown: 5,
   aliases: ['lf', 'lfm'],
-  description: 'Shows a user\'s Last.fm profile',
+  description: 'Provides information from the Last.fm API.',
   execute(message, args, cmd, client, Discord, profileData) {
-    if (args[0] != 'profile') return;
-
     let user = args[1];
     if (!user) return;
 
@@ -29,52 +27,56 @@ module.exports = {
       ))
       .then(data => {
         const getInfo = data[0].user;
-        const getRecentTracks = data[1].recenttracks.track[0];
-        const topArtist = data[2].topartists.artist[0].name;
-        const getTopAlbums = data[3].topalbums.album[0];
-        const getTopTracks = data[4].toptracks.track[0];
-        
-        var country = getInfo.country;
-        if (country == 'None') country = 'No country set'
-        const playcount = getInfo.playcount;
-        const image = JSON.parse(JSON.stringify(getInfo.image[2]))['#text'];
-        const registered = moment.unix(getInfo.registered.unixtime).format('DD MMM YYYY');
-        const username = getInfo.name;
-        const userURL = getInfo.url;
+        const getRecentTracks = data[1].recenttracks;
+        const getTopArtist = data[2].topartists;
+        const getTopAlbums = data[3].topalbums;
+        const getTopTracks = data[4].toptracks;
 
-        const songArtist = getRecentTracks.artist['#text'];
-        const songName = getRecentTracks.name;
-        const songURL = getRecentTracks.url;
-        const nowplaying = getRecentTracks['@attr'];
-        var listening = 'Listening' ;
-        var timeSince = ''
-        if (!nowplaying) {
-          listening = 'Was listening'
-          timeSince = `(${moment(getRecentTracks.date['#text'], 'DD MMM YYYY HH:mm').fromNow()})`;
+        if (args[0] == 'profile') {
+          var country = getInfo.country;
+          if (country == 'None') country = 'No country set';
+          const playcount = getInfo.playcount;
+          const image = JSON.parse(JSON.stringify(getInfo.image[2]))['#text'];
+          const registered = moment.unix(getInfo.registered.unixtime).format('DD MMM YYYY');
+          const username = getInfo.name;
+          const userURL = getInfo.url;
+
+          const songArtist = getRecentTracks.track[0].artist['#text'];
+          const songName = getRecentTracks.track[0].name;
+          const songURL = getRecentTracks.track[0].url;
+          const nowplaying = getRecentTracks.track[0]['@attr'];
+          var listening = 'Listening' ;
+          var timeSince = ''
+          if (!nowplaying) {
+            listening = 'Was listening'
+            timeSince = `(${moment(getRecentTracks.track[0].date['#text'], 'DD MMM YYYY HH:mm').fromNow()})`;
+          }
+
+          const topArtist = getTopArtist.artist[0].name;
+
+          const topAlbum = getTopAlbums.album[0].name;
+          const topAlbumArtist = getTopAlbums.album[0].artist.name;
+
+          const topTrack = getTopTracks.track[0].name;
+          const topTrackArtist = getTopTracks.track[0].artist.name;
+      
+          const embed = new Discord.MessageEmbed()
+          .setColor('RANDOM')
+          .setAuthor('Last.fm', 'https://images-ext-1.discordapp.net/external/LpyXpQXQ0yReqOQUyalm1-BpXTvW0LJbXtl57ShcRZg/https/i.imgur.com/pVu9vTr.png')
+          .setTitle(`${username}`)
+          .setURL(`${userURL}`)
+          .setDescription(`${listening} to: [**${songName}**](${songURL}) by **${songArtist}** ${timeSince}`)
+          .setThumbnail(image)
+          .addField('Country', country, true)
+          .addField('Scrobbles', playcount, true)
+          .addField('Registered', registered, true)
+          .addField('Top Artist', topArtist, true)
+          .addField('Top Album', `${topAlbum} by ${topAlbumArtist}`, true)
+          .addField('Top Track', `${topTrack} by ${topTrackArtist}`, true)
+          .setFooter('Powered by Last.fm')
+          .setTimestamp()
+          message.channel.send(message.author, embed);
         }
-
-        const topAlbum = getTopAlbums.name;
-        const topAlbumArtist = getTopAlbums.artist.name;
-
-        const topTrack = getTopTracks.name;
-        const topTrackArtist = getTopTracks.artist.name;
-    
-        const embed = new Discord.MessageEmbed()
-        .setColor('RANDOM')
-        .setAuthor('Last.fm', 'https://images-ext-1.discordapp.net/external/LpyXpQXQ0yReqOQUyalm1-BpXTvW0LJbXtl57ShcRZg/https/i.imgur.com/pVu9vTr.png')
-        .setTitle(`${username}`)
-        .setURL(`${userURL}`)
-        .setDescription(`${listening} to: [**${songName}**](${songURL}) by **${songArtist}** ${timeSince}`)
-        .setThumbnail(image)
-        .addField('Country', country, true)
-        .addField('Scrobbles', playcount, true)
-        .addField('Registered', registered, true)
-        .addField('Top Artist', topArtist, true)
-        .addField('Top Album', `${topAlbum} by ${topAlbumArtist}`, true)
-        .addField('Top Track', `${topTrack} by ${topTrackArtist}`, true)
-        .setFooter('Powered by Last.fm')
-        .setTimestamp()
-        message.channel.send(message.author, embed);
       }).catch((error) => console.log(error));    
 
       function parseJSON(response) {
